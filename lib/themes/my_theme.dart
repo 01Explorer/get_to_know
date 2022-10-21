cef33798d934b0b48db26dfe5fb535e6a59eaaac
Gabriel Santos <gabrielsantostca@gmail.com>
2022-10-21 14:37:03 -0300

    style(themes): Created the files to set the App's theme

---
+++ lib/themes/my_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:get_to_know/themes/theme_colors.dart';
+
+ThemeData goodLooking = ThemeData(
+  primarySwatch: ThemeColors.primaryColor,
+  primaryColor: ThemeColors.primaryColor,
+  brightness: Brightness.dark,
+);
