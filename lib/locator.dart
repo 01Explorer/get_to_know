b860a1380b2ae37a0c0d1a2d43a6bc8e80ec2b33
Gabriel Santos <gabrielsantostca@gmail.com>
2022-10-24 20:45:56 -0300

    feat(GetIt): Added the get it to work with dependency injection

---
+++ lib/locator.dart
@@ -0,0 +1,13 @@
+import 'package:get_it/get_it.dart';
+import 'package:get_to_know/http/webclients/search_webclient.dart';
+import 'package:get_to_know/controllers/albums_controller.dart';
+import 'package:get_to_know/controllers/artist_controller.dart';
+
+final locator = GetIt.instance;
+
+void setup() {
+  locator.registerSingletonAsync<SearchWebClient>(
+      () async => await SearchWebClient.createAsync());
+  locator.registerLazySingleton<ArtistController>(() => ArtistController());
+  locator.registerLazySingleton<AlbumsController>(() => AlbumsController());
+}
