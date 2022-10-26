fa72042cf6e58693a94f4fb185d1d84ea81ddb5a
Gabriel Santos <gabrielsantostca@gmail.com>
2022-10-26 10:12:41 -0300

    style(App): added the main font family

--- lib/themes/my_theme.dart
+++ lib/themes/my_theme.dart
@@ -5,17 +5,19 @@ ThemeData goodLooking = ThemeData(
   primarySwatch: ThemeColors.primaryColor,
   primaryColor: ThemeColors.primaryColor,
   brightness: Brightness.dark,
+  fontFamily: 'Poppins',
   textTheme: const TextTheme(
-      displaySmall: TextStyle(
-        fontSize: 40,
-        fontWeight: FontWeight.bold,
-        color: Colors.white,
-      ),
-      labelLarge: const TextStyle(
-        fontSize: 24,
-      ),
-      labelMedium: TextStyle(
-        fontSize: 16,
-        fontWeight: FontWeight.w600,
-      )),
+    displaySmall: TextStyle(
+      fontSize: 40,
+      fontWeight: FontWeight.bold,
+      color: Colors.white,
+    ),
+    labelLarge: TextStyle(
+      fontSize: 24,
+    ),
+    labelMedium: TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w500,
+    ),
+  ),
 );
