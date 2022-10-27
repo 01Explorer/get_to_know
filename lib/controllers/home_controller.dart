5f9e51fbc22dbd0fe295c3aae43a214c999ecfbe
Gabriel Santos <gabrielsantostca@gmail.com>
2022-10-26 23:43:00 -0300

    feat(home_controller): created a controller class to handle home

---
+++ lib/controllers/home_controller.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get_to_know/http/webclients/search_webclient.dart';
+import 'package:get_to_know/locator.dart';
+import 'package:get_to_know/models/album.dart';
+import 'package:get_to_know/models/artist.dart';
+
+class HomeController extends ChangeNotifier {
+  List<Album> itemsMusics = [];
+  List<Artist> itemsArtists = [];
+
+  Future<void> readJson() async {
+    final getInst = locator.get<SearchWebClient>();
+    final String responseMusic =
+        await rootBundle.loadString('assets/home_data.json');
+    final String responseArtists =
+        await rootBundle.loadString('assets/home_artists_data.json');
+    itemsMusics =
+        getInst.parseAlbumResponse(jsonDecode(responseMusic)["albums"]);
+    itemsArtists = getInst.parseArtistsResponse(jsonDecode(responseArtists));
+    notifyListeners();
+  }
+}
