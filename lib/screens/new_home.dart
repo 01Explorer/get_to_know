cacc4aefc36c1480701037d2d122b65177e8a192
Gabriel Santos <gabrielsantostca@gmail.com>
2022-10-27 00:34:57 -0300

    refactor(new_home): new_home code refactored with componentization

--- lib/screens/new_home.dart
+++ lib/screens/new_home.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get_to_know/components/sections/new_home_image_section.dart';
+import 'package:get_to_know/components/sections/new_home_new_release_section.dart';
+import 'package:get_to_know/components/sections/new_home_suggested_artists_section.dart';
+import 'package:get_to_know/components/standard_bottom_bar.dart';
 import 'package:get_to_know/controllers/home_controller.dart';
 import 'package:get_to_know/locator.dart';
-import 'package:get_to_know/screens/artist_screen.dart';
 
 class NewHome extends StatefulWidget {
   const NewHome({Key? key}) : super(key: key);
@@ -21,218 +24,17 @@ class _NewHomeState extends State<NewHome> {
           padding: const EdgeInsets.only(top: 64),
           child: Column(
             children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'Featured',
-                      style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 18,
-                      ),
-                    ),
-                    Icon(
-                      Icons.menu,
-                    )
-                  ],
-                ),
+              const NewHomeImageSection(
+                imagePath: 'assets/images/featured.png',
+                title: 'Featured',
               ),
-              SizedBox(
-                height: 8,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16),
-                child: Image.asset('assets/images/featured.png'),
-              ),
-              SizedBox(
-                height: 24,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'New Songs',
-                      style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 18,
-                      ),
-                    ),
-                    Text(
-                      'View All',
-                      style: TextStyle(
-                          fontSize: 12,
-                          fontWeight: FontWeight.w500,
-                          color: Color.fromRGBO(167, 167, 167, 1)),
-                    )
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 220,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: _controller.itemsMusics.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 16, vertical: 8),
-                      child: SizedBox(
-                        width: 148,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Container(
-                              height: 148,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(8),
-                                  image: DecorationImage(
-                                    image: NetworkImage(_controller
-                                        .itemsMusics[index].imagePath!),
-                                  )),
-                            ),
-                            Padding(
-                              padding: EdgeInsets.only(top: 8),
-                              child: Text(
-                                _controller.itemsMusics[index].name!,
-                                overflow: TextOverflow.ellipsis,
-                                style: TextStyle(
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                            ),
-                            Text(
-                              _controller.itemsMusics[index].artist!,
-                              overflow: TextOverflow.ellipsis,
-                              style: TextStyle(
-                                fontSize: 12,
-                                color: Color.fromRGBO(167, 167, 167, 1),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(16, 24, 16, 0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      'Suggested Artists',
-                      style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 18,
-                      ),
-                    ),
-                    Text(
-                      'View All',
-                      style: TextStyle(
-                          fontSize: 12,
-                          fontWeight: FontWeight.w500,
-                          color: Color.fromRGBO(167, 167, 167, 1)),
-                    )
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 200,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: _controller.itemsArtists.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 8.0, horizontal: 8),
-                      child: GestureDetector(
-                        onTap: () => Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => ArtistScreen(
-                                  selectedArtist:
-                                      _controller.itemsArtists[index]),
-                            )),
-                        child: SizedBox(
-                          width: 110,
-                          height: 129,
-                          child: Column(
-                            children: [
-                              SizedBox(
-                                height: 100,
-                                width: 110,
-                                child: Stack(
-                                  children: [
-                                    Container(
-                                      decoration: BoxDecoration(
-                                        shape: BoxShape.circle,
-                                        image: DecorationImage(
-                                          image: NetworkImage(_controller
-                                              .itemsArtists[index].imagePath!),
-                                        ),
-                                      ),
-                                    ),
-                                    Align(
-                                      alignment: Alignment.bottomRight,
-                                      child: Container(
-                                        height: 20,
-                                        width: 20,
-                                        decoration: BoxDecoration(
-                                          color: Colors.white,
-                                          shape: BoxShape.circle,
-                                        ),
-                                        child: Center(
-                                          child: Icon(
-                                            Icons.add,
-                                            color:
-                                                Color.fromRGBO(83, 83, 83, 1),
-                                            size: 20,
-                                          ),
-                                        ),
-                                      ),
-                                    )
-                                  ],
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(top: 8.0),
-                                child: Text(
-                                  _controller.itemsArtists[index].name!,
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w500,
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
+              const NewReleasesSection(title: 'New Releases'),
+              SuggestedArtistsSection(),
             ],
           ),
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.music_note), label: 'Library'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile')
-        ],
-        selectedItemColor: Colors.white,
-      ),
+      bottomNavigationBar: const StandardBottomBar(),
     );
   }
 }
