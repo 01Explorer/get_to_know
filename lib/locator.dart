d165a56deae395b9740efa64b81b43d524bf5389
Gabriel Santos <gabrielsantostca@gmail.com>
2022-10-26 23:16:56 -0300

    refactor(locator): since the main was changed there's no need be async

--- lib/locator.dart
+++ lib/locator.dart
@@ -3,13 +3,11 @@ import 'package:get_to_know/controllers/track_controller.dart';
 import 'package:get_to_know/http/webclients/search_webclient.dart';
 import 'package:get_to_know/controllers/albums_controller.dart';
 import 'package:get_to_know/controllers/artist_controller.dart';
-import 'package:get_to_know/models/track.dart';
 
 final locator = GetIt.instance;
 
 void setup() {
-  locator.registerSingletonAsync<SearchWebClient>(
-      () async => await SearchWebClient.createAsync());
+  locator.registerLazySingleton<SearchWebClient>(() => SearchWebClient());
   locator.registerLazySingleton<ArtistController>(() => ArtistController());
   locator.registerLazySingleton<AlbumsController>(() => AlbumsController());
   locator.registerLazySingleton<TrackController>(() => TrackController());
