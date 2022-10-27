b23568a960333ddf6bcfa2416bb4e1c83911f7d0
Gabriel Santos <gabrielsantostca@gmail.com>
2022-10-27 00:38:19 -0300

    style(loading): removed the background color

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:get_to_know/components/background_container.dart';
 
 class Loading extends StatelessWidget {
   const Loading({Key? key}) : super(key: key);
@@ -8,20 +7,16 @@ class Loading extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BackgroundContainer(
-        ClipRRect(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: const [
-              SpinKitFadingCube(
-                color: Colors.white,
-                size: 50,
-                duration: Duration(milliseconds: 700),
-              )
-            ],
-          ),
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: const [
+          SpinKitFadingCube(
+            color: Colors.white,
+            size: 50,
+            duration: Duration(milliseconds: 700),
+          )
+        ],
       ),
     );
   }
