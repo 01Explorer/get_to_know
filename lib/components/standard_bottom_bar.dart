bbd5cef0551a6236153d5bd26c9be418135e8d64
Gabriel Santos <gabrielsantostca@gmail.com>
2022-10-27 00:33:59 -0300

    refactor(app): created the standard bottom navigation bar

---
+++ lib/components/standard_bottom_bar.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class StandardBottomBar extends StatelessWidget {
+  const StandardBottomBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+        BottomNavigationBarItem(icon: Icon(Icons.music_note), label: 'Library'),
+        BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile')
+      ],
+      selectedItemColor: Colors.white,
+    );
+  }
+}
